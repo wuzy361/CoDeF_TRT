9308d844c3f643ca899de4d28a736accd65561db
qiuyu <qiuyu96@mail.ustc.edu.cn>
2023-08-25 11:40:19 +0800

    Fix mask preprocessing

--- data_preprocessing/preproc_mask.py
+++ data_preprocessing/preproc_mask.py
@@ -9,10 +9,10 @@ name = 'beauty_1'
 
 msk_folder = f'{root_dir}/{name}/{name}_masks'
 img_folder = f'{root_dir}/{name}/{name}'
-frg_folder = f'{root_dir}/{name}/{name}_masks_0'
-bkg_folder = f'{root_dir}/{name}/{name}_masks_1'
-os.makedirs(frg_folder, exist_ok=True)
-os.makedirs(bkg_folder, exist_ok=True)
+frg_mask_folder = f'{root_dir}/{name}/{name}_masks_0'
+bkg_mask_folder = f'{root_dir}/{name}/{name}_masks_1'
+os.makedirs(frg_mask_folder, exist_ok=True)
+os.makedirs(bkg_mask_folder, exist_ok=True)
 
 files = glob(msk_folder + '/*.png')
 num = len(files)
@@ -21,10 +21,10 @@ for i in tqdm(range(num)):
     file_n = os.path.basename(files[i])
     mask = cv2.imread(os.path.join(msk_folder, file_n), 0)
     mask[mask > 0] = 1
-    frg = cv2.imread(os.path.join(img_folder,
-                                    file_n[:-4] + ".png")) * mask[:, :, None]
-    bkg = cv2.imread(os.path.join(
-        img_folder, file_n[:-4] + ".png")) * (1 - mask[:, :, None])
-    cv2.imwrite(os.path.join(msk_folder, file_n), mask * 255)
-    cv2.imwrite(os.path.join(frg_folder, file_n), frg)
-    cv2.imwrite(os.path.join(bkg_folder, file_n), bkg)
+    cv2.imwrite(os.path.join(frg_mask_folder, file_n), mask * 255)
+
+    bg_mask = mask.copy()
+    bg_mask[bg_mask == 0] = 127
+    bg_mask[bg_mask == 255] = 0
+    bg_mask[bg_mask == 127] = 255
+    cv2.imwrite(os.path.join(bkg_mask_folder, file_n), bg_mask)
